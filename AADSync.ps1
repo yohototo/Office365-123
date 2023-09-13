19e8a8d6aa94de7486cc21558e7f58fcf913c2f1
yohototo <35169273+yohototo@users.noreply.github.com>
2023-09-13 08:36:01 +0800

    Update AADSync.ps1

--- AADSync.ps1
+++ AADSync.ps1
@@ -6,3 +6,6 @@ $session = New-PSSession -ComputerName $AADComputer
 Invoke-Command -Session $session -ScriptBlock {Import-Module -Name 'ADSync'}
 Invoke-Command -Session $session -ScriptBlock {Start-ADSyncSyncCycle -PolicyType Delta}
 Remove-PSSession $session
+
+$session = New-PSSession -ComputerName $AADComputer
+
